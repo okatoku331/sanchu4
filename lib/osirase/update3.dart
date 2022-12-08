faa25e8f615eccc504dfa19e8cfea1c7feac0e47
oka-teruhiro <oka331@gmail.com>
2022-12-09 02:09:51 +0900

    第10版　FireBase 導入 update3ページ動作（2022.7.14 15:12)

---
+++ lib/osirase/update3.dart
@@ -0,0 +1,84 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Update3 extends StatelessWidget {
+  Update3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('update').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return const Text('Loading...');
+          final List<String> lineList = [
+            'line1',
+            'line2',
+            'line3',
+            'line4',
+            'line5',
+            'line6',
+            'line7',
+            'line8',
+            'line9',
+            'line10',
+            'line11',
+            'line12',
+            'line13',
+            'line14',
+            'line15',
+            'line16',
+            'line17',
+            'line18',
+            'line19',
+            'line20',
+            'line21',
+            'line22',
+            'line23',
+            'line24',
+            'line25',
+            'line26',
+            'line27',
+            'line28',
+            'line29',
+            'line30',
+            'line31',
+            'line32',
+            'line33',
+            'line34',
+            'line35',
+            'line36',
+            'line37',
+            'line38',
+            'line39',
+            'line40',
+            'line47',
+            'line41',
+            'line42',
+            'line43',
+            'line44',
+            'line45',
+            'line46',
+          ];
+          //final List<String> lineList = snapshot.data.docs[0]['aLine'];
+          return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              title: Text(
+                snapshot.data!.docs[0]['line0'],
+                style: const TextStyle(
+                  color: Colors.pinkAccent,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            body: ListView.builder(
+                itemCount: lineList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(snapshot.data!.docs[0][lineList[index]]),
+                  );
+                }),
+          );
+        });
+  }
+}
